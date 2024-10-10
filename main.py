5322ca404ed11daf1f4eabf18c4d06fee74c639f
Александр Розин <zak-24@yandex.ru>
2024-10-10 12:37:33 +0500

    Добавлена возможность сохранения ссылок в файл json для дальнейшего использования

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from tkinter import messagebox as mb
 from tkinter import filedialog as fd
 from tkinter import ttk
 import pyperclip
+import json
+import os
+
 
 def upload():
     try:
@@ -14,8 +17,10 @@ def upload():
                 response = requests.post('https://file.io', files= files)
                 response.raise_for_status()
                 download_link = response.json()['link']
+                # entry.delete(0, END)
                 entry.insert(0, download_link)
                 pyperclip.copy(download_link)
+                save_history(filepath, download_link)
                 mb.showinfo('Успешно!','Ссылка успешно скопирована')
         else:
             raise ValueError('Не удалось отправить файл')
@@ -24,6 +29,21 @@ def upload():
     except Exception as e:
         mb.showerror('Ошибка!', f'Произошла ошибка {e}')
 
+def save_history(file_path, download_link):
+    hist_list = []
+    if os.path.exists(history_file):
+        with open(history_file, 'r') as file:
+            history = json.load(file)
+
+    hist_list.append({'file_path': os.path.basename(file_path),
+                      'download_link': download_link})
+    with open(history_file, 'w') as file:
+        json.dump(hist_list, file, indent = 4)
+
+
+
+
+history_file = 'History.json'
 
 w = Tk()
 w.title('Файл в облако')
